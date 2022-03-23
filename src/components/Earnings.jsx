02a35bf1c2cf46b09661ae228a3aab1c9ab51346
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-24 00:50:17 +0100

    done

--- src/components/Earnings.jsx
+++ src/components/Earnings.jsx
@@ -1,162 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
+import Cart from "./Cart";
 import { cardStyles } from "./ReuseableStyles";
-const data = [
-  { data: 4500 },
-  {
-    data: 5000,
-  },
-  {
-    data: 4700,
-  },
-  {
-    data: 4400,
-  },
-  {
-    data: 4800,
-  },
-  {
-    data: 5300,
-  },
-  {
-    data: 5800,
-  },
-  {
-    data: 6000,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 6580,
-  },
-  {
-    data: 6780,
-  },
-  {
-    data: 6680,
-  },
-  {
-    data: 6500,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 5900,
-  },
-  {
-    data: 5700,
-  },
-  {
-    data: 5500,
-  },
-  {
-    data: 5300,
-  },
-  {
-    data: 5100,
-  },
-  {
-    data: 5090,
-  },
-  {
-    data: 5300,
-  },
-  {
-    data: 5800,
-  },
-  {
-    data: 6000,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 6780,
-  },
-  {
-    data: 6500,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 6500,
-  },
-  {
-    data: 6700,
-  },
-  {
-    data: 7000,
-  },
-  {
-    data: 7300,
-  },
-  {
-    data: 7500,
-  },
-  {
-    data: 7700,
-  },
-  {
-    data: 8090,
-  },
-  {
-    data: 8190,
-  },
-  {
-    data: 7990,
-  },
 
-  {
-    data: 7700,
-  },
-  {
-    data: 7500,
-  },
-  {
-    data: 7300,
-  },
-  {
-    data: 7000,
-  },
-  {
-    data: 6700,
-  },
-  {
-    data: 6500,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 6500,
-  },
-  {
-    data: 6780,
-  },
-  {
-    data: 6300,
-  },
-  {
-    data: 6000,
-  },
-  {
-    data: 5800,
-  },
-
-  {
-    data: 5490,
-  },
-  {
-    data: 6000,
-  },
-  {
-    data: 8000,
-  },
-];
 export default function Earnings() {
   return (
     <Section>
@@ -169,27 +15,9 @@ export default function Earnings() {
           </div>
         </div>
       </div>
-      <div className="chart">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
-          >
-            <Tooltip cursor={false} />
-            <Area
-              animationBegin={800}
-              animationDuration={2000}
-              type="monotone"
-              dataKey="data"
-              stroke="#ffc107"
-              fill="#8068233e"
-              strokeWidth={4}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
+      {/* <div className="chart">
+        <Cart />
+      </div> */}
     </Section>
   );
 }
@@ -198,7 +26,7 @@ const Section = styled.section`
   flex-direction: column;
   justify-content: space-between;
   min-height: 20rem;
-  ${cardStyles}
+  ${cardStyles};
   padding: 2rem 0 0 0;
   .top {
     .info {
