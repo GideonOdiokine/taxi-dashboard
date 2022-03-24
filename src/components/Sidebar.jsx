9f331ddd6c3489121f626e753916150b61f8a2b1
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-25 00:50:34 +0100

    added media query

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, { useState,} from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { MdSpaceDashboard } from "react-icons/md";
 import { RiDashboard2Fill } from "react-icons/ri";
@@ -8,11 +8,15 @@ import { GiTwirlCenter } from "react-icons/gi";
 import { BsFillChatTextFill } from "react-icons/bs";
 import { IoSettings } from "react-icons/io5";
 import { FiLogOut } from "react-icons/fi";
-// import { GiHamburgerMenu } from "react-icons/gi";
-// import { VscChromeClose } from "react-icons/vsc";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Sidebar = () => {
   const [currentLink, setCurrentLink] = useState(1);
+  const [navbarState, setNavbarState] = useState(false);
+  const html = document.querySelector("html");
+  html.addEventListener("click", () => setNavbarState(false));
+
   return (
     <>
       <Section>
@@ -21,14 +25,25 @@ const Sidebar = () => {
             <FaTaxi />
             <span>My Taxi</span>
           </div>
-          <div className="toggle"></div>
+          <div className="toggle">
+            {navbarState ? (
+              <VscChromeClose onClick={() => setNavbarState(false)} />
+            ) : (
+              <GiHamburgerMenu
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setNavbarState(true);
+                }}
+              />
+            )}
+          </div>
           <div className="links">
             <ul>
               <li
                 onClick={() => setCurrentLink(1)}
                 className={currentLink === 1 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <MdSpaceDashboard />
                   <span>Dashboard</span>
                 </Link>
@@ -37,7 +52,7 @@ const Sidebar = () => {
                 onClick={() => setCurrentLink(2)}
                 className={currentLink === 2 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <RiDashboard2Fill />
                   <span>Riders</span>
                 </Link>
@@ -46,7 +61,7 @@ const Sidebar = () => {
                 onClick={() => setCurrentLink(3)}
                 className={currentLink === 3 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <FaAddressCard />
                   <span>Payment Details</span>
                 </Link>
@@ -55,7 +70,7 @@ const Sidebar = () => {
                 onClick={() => setCurrentLink(4)}
                 className={currentLink === 4 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <GiTwirlCenter />
                   <span>Learning Center</span>
                 </Link>
@@ -64,7 +79,7 @@ const Sidebar = () => {
                 onClick={() => setCurrentLink(5)}
                 className={currentLink === 5 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <BsFillChatTextFill />
                   <span>FAQ</span>
                 </Link>
@@ -73,7 +88,7 @@ const Sidebar = () => {
                 onClick={() => setCurrentLink(6)}
                 className={currentLink === 6 ? "active" : ""}
               >
-                <Link to="/" >
+                <Link to="/">
                   <IoSettings />
                   <span>Settings</span>
                 </Link>
@@ -88,6 +103,66 @@ const Sidebar = () => {
           </Link>
         </div>
       </Section>
+      <ResponsiveNav state={navbarState} className={navbarState ? "show" : ""}>
+        <div className="responsive__links">
+          <ul>
+            <li
+              onClick={() => setCurrentLink(1)}
+              className={currentLink === 1 ? "active" : ""}
+            >
+              <Link to="/">
+                <MdSpaceDashboard />
+                <span>Dashboard</span>
+              </Link>
+            </li>
+            <li
+              onClick={() => setCurrentLink(2)}
+              className={currentLink === 2 ? "active" : ""}
+            >
+              <Link to="/">
+                <RiDashboard2Fill />
+                <span>Riders</span>
+              </Link>
+            </li>
+            <li
+              onClick={() => setCurrentLink(3)}
+              className={currentLink === 3 ? "active" : ""}
+            >
+              <Link to="/">
+                <FaAddressCard />
+                <span>Payment Details</span>
+              </Link>
+            </li>
+            <li
+              onClick={() => setCurrentLink(4)}
+              className={currentLink === 4 ? "active" : ""}
+            >
+              <Link to="/">
+                <GiTwirlCenter />
+                <span>Learning Center</span>
+              </Link>
+            </li>
+            <li
+              onClick={() => setCurrentLink(5)}
+              className={currentLink === 5 ? "active" : ""}
+            >
+              <Link to="/">
+                <BsFillChatTextFill />
+                <span>FAQ</span>
+              </Link>
+            </li>
+            <li
+              onClick={() => setCurrentLink(6)}
+              className={currentLink === 6 ? "active" : ""}
+            >
+              <Link to="/">
+                <IoSettings />
+                <span>Settings</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </ResponsiveNav>
     </>
   );
 };
@@ -170,6 +245,75 @@ const Section = styled.section`
       align-items: center;
       justify-content: flex-start;
       color: #fff;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    position: initial;
+    width: 100%;
+    height: max-content;
+    padding: 1rem;
+    .top {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 1rem;
+      .toggle {
+        display: block;
+        color: white;
+        z-index: 100;
+        svg {
+          font-size: 1.4rem;
+        }
+      }
+      .brand {
+        gap: 1rem;
+        /* justify-content: flex-end; */
+        width: auto;
+      }
+    }
+    .top > .links,
+    .logout {
+      display: none;
+    }
+  }
+`;
+
+const ResponsiveNav = styled.nav`
+  position: fixed;
+  right: -10vw;
+  top: 0;
+  height: 100vh;
+  background-color: black;
+  width: ${({ state }) => (state ? "60%" : "0%")};
+
+  transition: 0.4s ease-in-out;
+  /* opacity: 0;
+  visibility: hidden; */
+  padding: 1rem;
+  .responsive__links {
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      li {
+        padding: 0.6rem 1rem;
+        a {
+          display: flex;
+          align-items: center;
+          color: #fff;
+          span {
+            padding-left: 8px;
+          }
+        }
+      }
+      .active {
+        background-color: #ffc107;
+        border-radius: 2px;
+        a {
+          color: #000;
+        }
+      }
     }
   }
 `;
